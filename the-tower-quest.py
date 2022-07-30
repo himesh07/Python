9dec2f76d44c63181334a4895adefcc99c1d7799
himesh07 <hich7248@colorado.edu>
2022-07-30 17:24:38 -0400

    The Tower Quest added
    Mission is to save princess in the tower.

---
+++ the-tower-quest.py
@@ -0,0 +1,46 @@
+print('''
+*******************************************************************************
+    *                             |>>>                    +
++          *                      |                   *       +
+                    |>>>      _  _|_  _   *     |>>>
+           *        |        |;| |;| |;|        |                 *
+     +          _  _|_  _    \\.    .  /    _  _|_  _       +
+ *             |;|_|;|_|;|    \\: +   /    |;|_|;|_|;|
+               \\..      /    ||:+++. |    \\.    .  /           *
+      +         \\.  ,  /     ||:+++  |     \\:  .  /
+                 ||:+  |_   _ ||_ . _ | _   _||:+  |       *
+          *      ||+++.|||_|;|_|;|_|;|_|;|_|;||+++ |          +
+                 ||+++ ||.    .     .      . ||+++.|   *
++   *            ||: . ||:.     . .   .  ,   ||:   |               *
+         *       ||:   ||:  ,     +       .  ||: , |      +
+  *              ||:   ||:.     +++++      . ||:   |         *
+     +           ||:   ||.     +++++++  .    ||: . |    +
+           +     ||: . ||: ,   +++++++ .  .  ||:   |             +
+                 ||: . ||: ,   +++++++ .  .  ||:   |        *
+                 ||: . ||: ,   +++++++ .  .  ||:   |
+
+*******************************************************************************
+''')
+print("Welcome to The Tower Quest")
+print("Your mission is to save the princess.")
+
+#Flow Chart
+#Write your code below this line 👇
+
+choice1 = input('You\'re at the gate, there\'s a cross road ahead. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake surrounding the tower. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the tower unharmed. There are 3 stairs leading upstairs. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+      print("You found the princess! You Win!")
+    elif choice3 == "blue":
+      print("You enter a room of beasts. Game Over.")
+    else:
+      print("You chose a door that doesn't exist. Game Over.")
+  else:
+    print("You get attacked by an angry alligator. Game Over.")
+else:
+  print("You fell into a hole. Game Over.")
